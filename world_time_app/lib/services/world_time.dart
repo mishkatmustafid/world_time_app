10d66b4432bf4bd82b6550e6b193d58ae508ab2b
mishkatmustafid <mishkatmustafid@gmail.com>
2020-05-09 00:36:12 +0600

    Error handling

--- world_time_app/lib/services/world_time.dart
+++ world_time_app/lib/services/world_time.dart
@@ -11,23 +11,32 @@ class WorldTime {
   WorldTime({this.location, this.flag, this.url});
 
   Future<void> getTime() async{
-    // make the request
-    Response response = await get('http://worldtimeapi.org/api/timezone/$url');
-    Map data = jsonDecode(response.body);
-    //print(data);
 
-    // get properties from data
-    String datetime = data['utc_datetime'];           // We can use 'datetime' directly to get local time
-    String offset = data['utc_offset'].substring(1,3);// But lets just follow the tutorial for now.
-    //print(datetime);
-    //print(offset);
+    try{
+      // make the request
+      Response response = await get('http://worldtimeapi.org/api/timezone/$url');
+      Map data = jsonDecode(response.body);
+      //print(data);
 
-    // create Datetime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(new Duration(hours: int.parse(offset)));
+      // get properties from data
+      String datetime = data['utc_datetime'];           // We can use 'datetime' directly to get local time
+      String offset = data['utc_offset'].substring(1,3);// But lets just follow the tutorial for now.
+      //print(datetime);
+      //print(offset);
 
-    // set the time property
-    time = now.toString();
+      // create Datetime object
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(new Duration(hours: int.parse(offset)));
+
+      // set the time property
+      time = now.toString();
+
+
+    }
+    catch(e){
+      print('Caught error : $e');
+      time = 'Could not get the time data';
+    }
 
 
   }
